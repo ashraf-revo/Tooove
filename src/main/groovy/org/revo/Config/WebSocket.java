c9221d33d3065668999eff6acb83875e61a14c38
ashraf <ashraf1abdelrasool@gmail.com>
2016-11-18 13:00:30 +0200

    Initial commit

---
+++ src/main/groovy/org/revo/Config/WebSocket.java
@@ -0,0 +1,29 @@
+package org.revo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * Created by ashraf on 06/09/16.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocket extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+//        registry.enableStompBrokerRelay("/topic/")
+    }
+
+//see if you will use jwt http://stackoverflow.com/questions/30887788/json-web-token-jwt-with-spring-based-sockjs-stomp-web-socket
+}
