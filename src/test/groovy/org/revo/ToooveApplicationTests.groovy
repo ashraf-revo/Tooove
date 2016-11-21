e56367fea63ac26b8a73b8bfb4fe8fdc7fb94ed6
ashraf <ashraf1abdelrasool@gmail.com>
2016-11-21 20:58:43 +0200

    edit5

--- src/test/groovy/org/revo/ToooveApplicationTests.groovy
+++ src/test/groovy/org/revo/ToooveApplicationTests.groovy
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class ToooveApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
